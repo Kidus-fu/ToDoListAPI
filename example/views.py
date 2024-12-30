a36e2206c7bb0051815198d1c0aa687acd5092b9
kidus surafel <seeh51593@gmail.com>
2024-12-31 00:03:25 +0300

    .

--- example/views.py
+++ example/views.py
@@ -15,7 +15,7 @@ def index(request):
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <p>T.</p>
         </body>
     </html>
     '''
