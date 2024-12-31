7cda43a85ec67ebfc8cf38ee09d2c3ba51dcd9bb
kidus surafel <seeh51593@gmail.com>
2025-01-01 01:22:43 +0300

    Media file

--- api/settings.py
+++ api/settings.py
@@ -70,9 +70,10 @@ WSGI_APPLICATION = 'api.wsgi.app'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Ensure this path is correct
     }
 }
+
 
 
 
@@ -112,7 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
